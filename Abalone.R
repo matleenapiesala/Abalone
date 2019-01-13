265dd1c0b6b3addcbc699b2c60e857ded48f7aae
Miia Ketolainen <miia.ketolainen@eatech.fi>
2019-01-13 19:09:59 +0200

    Removed comments and added cleaning

--- Abalone.R
+++ Abalone.R
@@ -1,7 +1,5 @@
-
-
-# Read in the hospital data
-#read file from folder. Expects to have only one file.
+rm(list = ls())
 datalocation <- "./data/"
 AbaloneDataPath <- paste(datalocation, "abalone.data", sep="")
 AbaloneData <- read.csv(AbaloneDataPath, header = TRUE, sep = ",", na.strings="Not Available")
+
